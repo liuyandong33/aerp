e8a4867150dac9c68c5b2bb667674c15e26860cf
liuyandong <554030404@qq.com>
2019-08-02 17:55:30 +0800

    整合推送系统！！

--- app/src/main/java/build/dream/aerp/activities/MainActivity.java
+++ app/src/main/java/build/dream/aerp/activities/MainActivity.java
@@ -3,6 +3,10 @@ package build.dream.aerp.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.alibaba.sdk.android.push.CloudPushService;
 import com.alibaba.sdk.android.push.CommonCallback;
@@ -12,22 +16,37 @@ import build.dream.aerp.R;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private EditText loginNameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CloudPushService cloudPushService = PushServiceFactory.getCloudPushService();
-        cloudPushService.bindAccount("61011888", new CommonCallback() {
-            @Override
-            public void onSuccess(String s) {
-                Log.i(TAG, s);
-            }
+        loginNameEditText = findViewById(R.id.activity_main_edit_text_login_name);
+        passwordEditText = findViewById(R.id.activity_main_edit_text_password);
+        loginButton = findViewById(R.id.activity_main_button_login_button);
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFailed(String s, String s1) {
-                Log.i(TAG, s + "-" + s1);
+            public void onClick(View view) {
+                CloudPushService cloudPushService = PushServiceFactory.getCloudPushService();
+                cloudPushService.bindAccount("61011888", new CommonCallback() {
+                    @Override
+                    public void onSuccess(String s) {
+                        Log.i(TAG, s);
+                    }
+
+                    @Override
+                    public void onFailed(String s, String s1) {
+                        Log.i(TAG, s + "-" + s1);
+                    }
+                });
+
+                Toast.makeText(MainActivity.this, "DeviceId: " + cloudPushService.getDeviceId(), Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "UTDeviceId: " + cloudPushService.getUTDeviceId(), Toast.LENGTH_LONG).show();
             }
         });
     }
