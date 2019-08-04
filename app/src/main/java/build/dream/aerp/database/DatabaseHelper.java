d21e22610753eb0996ad0552d15165d29bb2d590
liuyandong <554030404@qq.com>
2019-08-04 19:56:00 +0800

    初始化仓库！！

--- app/src/main/java/build/dream/aerp/database/DatabaseHelper.java
+++ app/src/main/java/build/dream/aerp/database/DatabaseHelper.java
@@ -7,10 +7,16 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -18,7 +24,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String SQL_SCRIPT_FILE_NAME = "sql/aerp-db-sql.xml";
     private static final String DATABASE_NAME = "aerp-db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 4;
     private Context context;
 
     public DatabaseHelper(Context context) {
@@ -26,13 +32,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         this.context = context;
     }
 
-    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory) {
+        super(context, name, factory, DATABASE_VERSION);
         this.context = context;
     }
 
-    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version, @Nullable DatabaseErrorHandler errorHandler) {
-        super(context, name, factory, version, errorHandler);
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, @Nullable DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, DATABASE_VERSION, errorHandler);
         this.context = context;
     }
 
@@ -47,11 +53,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
             NodeList onCreateSqlNodeList = rootElement.getElementsByTagName("on.create.sql");
             if (onCreateSqlNodeList.getLength() > 0) {
-                NodeList childNodes = onCreateSqlNodeList.item(0).getChildNodes();
-                int length = childNodes.getLength();
+                NodeList nodeList = onCreateSqlNodeList.item(0).getChildNodes();
+                int length = nodeList.getLength();
                 if (length > 0) {
                     for (int index = 0; index < length; index++) {
-                        Node node = childNodes.item(index);
+                        Node node = nodeList.item(index);
                         if (node.getNodeType() != Node.ELEMENT_NODE) {
                             continue;
                         }
@@ -65,7 +71,49 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        try {
+            AssetManager assetManager = context.getAssets();
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(assetManager.open(SQL_SCRIPT_FILE_NAME));
+            Element rootElement = document.getDocumentElement();
 
+            NodeList onUpgradeSqlNodeList = rootElement.getElementsByTagName("on.upgrade.sql");
+            if (onUpgradeSqlNodeList.getLength() <= 0) {
+                return;
+            }
+
+            Map<String, List<String>> sqlMap = new LinkedHashMap<String, List<String>>();
+            NodeList nodeList = onUpgradeSqlNodeList.item(0).getChildNodes();
+            for (int index = 0; index < nodeList.getLength(); index++) {
+                Node node = nodeList.item(index);
+                if (node.getNodeType() != Node.ELEMENT_NODE) {
+                    continue;
+                }
+
+                NodeList childNodes = node.getChildNodes();
+                List<String> sqlList = new ArrayList<String>();
+                for (int childNodesIndex = 0; childNodesIndex < childNodes.getLength(); childNodesIndex++) {
+                    Node childNode = childNodes.item(childNodesIndex);
+                    if (childNode.getNodeType() != Node.ELEMENT_NODE) {
+                        continue;
+                    }
+                    sqlList.add(childNode.getTextContent().trim());
+                }
+                sqlMap.put(node.getNodeName(), sqlList);
+            }
+            for (int version = oldVersion + 1; version <= newVersion; version++) {
+                List<String> sqlList = sqlMap.get("version." + version);
+                if (CollectionUtils.isEmpty(sqlList)) {
+                    continue;
+                }
+                for (String sql : sqlList) {
+                    sqLiteDatabase.execSQL(sql);
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("初始化数据库失败！");
+        }
     }
 }
